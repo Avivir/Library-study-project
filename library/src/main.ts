ac77164ef1534478b95f7480a98a040f49928683
Avivir <96440429+Avivir@users.noreply.github.com>
2023-07-05 04:42:56 +0200

    w module main.ts będziemy wrzucać książki, które trafią do local storage po uruchomieniu na ten moment jest to zakomentowane żeby nie wkurzało podczas testowania

--- library/src/main.ts
+++ library/src/main.ts
@@ -1,7 +1,38 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { enableProdMode } from '@angular/core';
 import { AppModule } from './app/app.module';
 
+enableProdMode();
+
+loadDataToLocalStorage();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+function loadDataToLocalStorage() {
+  // Przykładowa lista książek
+  const books = [
+    /*
+    { id: 1, title: 'Książka 1', description: 'Opis książki 1', author: 'Autor 1', file: 'path/do/1.jpg' },
+    { id: 2, title: 'Książka 2', description: 'Opis książki 2', author: 'Autor 2', file: 'path/do/2.jpg' },
+    { id: 3, title: 'Książka 3', description: 'Opis książki 3', author: 'Autor 3', file: 'path/do/3.jpg' },
+    { id: 3, title: 'Książka 3', description: 'Opis książki 3', author: 'Autor 3', file: 'path/do/3.jpg' },
+    { id: 3, title: 'Książka 3', description: 'Opis książki 3', author: 'Autor 3', file: 'path/do/3.jpg' },
+    { id: 3, title: 'Książka 3', description: 'Opis książki 3', author: 'Autor 3', file: 'path/do/3.jpg' },
+    { id: 3, title: 'Książka 3', description: 'Opis książki 3', author: 'Autor 3', file: 'path/do/3.jpg' },
+    { id: 3, title: 'Książka 3', description: 'Opis książki 3', author: 'Autor 3', file: 'path/do/3.jpg' },
+    { id: 3, title: 'Książka 3', description: 'Opis książki 3', author: 'Autor 3', file: 'path/do/3.jpg' },
+    { id: 1, title: 'Książka 1', description: 'Opis książki 1', author: 'Autor 1', file: 'path/do/1.jpg' },
+    { id: 2, title: 'Książka 2', description: 'Opis książki 2', author: 'Autor 2', file: 'path/do/2.jpg' },
+    { id: 3, title: 'Książka 3', description: 'Opis książki 3', author: 'Autor 3', file: 'path/do/3.jpg' },
+    { id: 3, title: 'Książka 3', description: 'Opis książki 3', author: 'Autor 3', file: 'path/do/3.jpg' },
+    { id: 3, title: 'Książka 3', description: 'Opis książki 3', author: 'Autor 3', file: 'path/do/3.jpg' },
+    { id: 3, title: 'Książka 3', description: 'Opis książki 3', author: 'Autor 3', file: 'path/do/3.jpg' },
+    { id: 3, title: 'Książka 3', description: 'Opis książki 3', author: 'Autor 3', file: 'path/do/3.jpg' },
+    { id: 3, title: 'Książka 3', description: 'Opis książki 3', author: 'Autor 3', file: 'path/do/3.jpg' },
+    { id: 3, title: 'Książka 3', description: 'Opis książki 3', author: 'Autor 3', file: 'path/do/3.jpg' }
+     */
+  ];
+
+  //localStorage.setItem('books', JSON.stringify(books));
+}
